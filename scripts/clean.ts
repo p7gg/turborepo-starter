f15c3209083f4615f4bb86c78787cd7746ebc22a
p7gg <69444613+p7gg@users.noreply.github.com>
2023-02-20 15:31:06 -0300

    chore: add clean script

---
+++ scripts/clean.ts
@@ -0,0 +1,40 @@
+/* eslint-disable no-console */
+import * as c from 'colorette'
+import { lstat, pathExists, remove } from 'fs-extra'
+
+const root = process.cwd()
+const pkgJson = findClosestPackageJson(root)
+const dirsToClean = process.argv.slice(2)
+
+import { findClosestPackageJson, r } from './utils'
+void (async () => {
+	try {
+		if (!dirsToClean.length) {
+			throw new Error('Please provide directories to clean up.')
+		}
+
+		const promises: Promise<string>[] = []
+
+		for (const dir of dirsToClean) {
+			const fullPath = r(root, dir)
+
+			const dirExists = await pathExists(fullPath)
+
+			if (!dirExists) continue
+
+			const stats = await lstat(fullPath)
+
+			if (!stats.isDirectory()) {
+				throw new Error(`The provided path is not a directory: ${fullPath}`)
+			}
+
+			promises.push(remove(fullPath).then(() => dir))
+		}
+
+		await Promise.all(promises)
+
+		console.log(`Successfully cleaned workspace: ${c.green(pkgJson.name)}`)
+	} catch (error) {
+		console.error(`An error occurred while cleaning up workspace: ${c.red(pkgJson.name)}`)
+	}
+})()
---
+++ scripts/utils.ts
@@ -0,0 +1,15 @@
+import { readFileSync } from 'fs-extra'
+import { dirname, resolve } from 'node:path'
+
+export const r = (...pathSegments: string[]) => resolve(__dirname, ...pathSegments)
+
+export const findClosestPackageJson = (start: string, level = 0): any => {
+	try {
+		const path = resolve(start, 'package.json')
+		const content = readFileSync(path, { encoding: 'utf8' })
+
+		return JSON.parse(content)
+	} catch {
+		return level >= 10 ? {} : findClosestPackageJson(dirname(start), level + 1)
+	}
+}
