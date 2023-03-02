f88ec5a0b41458ba941c63ef75e6f15e24a9c6a7
p7gg <69444613+p7gg@users.noreply.github.com>
2023-03-02 19:56:38 -0300

    chore: add stricter tsconfig options

--- apps/docs/src/App.tsx
+++ apps/docs/src/App.tsx
@@ -5,14 +5,14 @@ import type { Component } from 'solid-js'
 
 const App: Component = () => {
 	return (
-		<div class={styles.App}>
-			<header class={styles.header}>
-				<img src={logo} class={styles.logo} alt="logo" />
+		<div class={styles['App']}>
+			<header class={styles['header']}>
+				<img src={logo} class={styles['logo']} alt="logo" />
 				<p>
 					Edit <code>src/App.tsx</code> and save to reload.
 				</p>
 				<a
-					class={styles.link}
+					class={styles['link']}
 					href="https://github.com/solidjs/solid"
 					target="_blank"
 					rel="noopener noreferrer"
