6a22ddce0b851c05d08b7ba3876c68738de9d66c
Gabriel Silva <69444613+p7gg@users.noreply.github.com>
2023-03-07 17:01:22 -0300

    chore: split main component from entry point

--- packages/core/dev/index.tsx
+++ packages/core/dev/index.tsx
@@ -1,20 +1,6 @@
 /* @refresh reload */
-import { createSignal } from 'solid-js'
 import { render } from 'solid-js/web'
 
-import type { Component } from 'solid-js'
-
-const App: Component = () => {
-	const [count, setCount] = createSignal(0)
-	const increment = () => setCount(count() + 1)
-
-	return (
-		<div>
-			<h4>Counter component</h4>
-			<p>it's very important...</p>
-			<button onClick={increment}>{count()}</button>
-		</div>
-	)
-}
+import { App } from './App'
 
 render(() => <App />, document.querySelector('#root') as HTMLElement)
