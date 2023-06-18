6549582eaf28fda8e95efbae329415d48440dcc4
Gabriel Silva <69444613+p7gg@users.noreply.github.com>
2023-06-17 21:18:53 -0300

    chore: format

--- packages/config/eslint/next.js
+++ packages/config/eslint/next.js
@@ -1,11 +1,10 @@
 /** @type {import('eslint').Linter.Config} */
 const config = {
-  extends: ["plugin:@next/next/recommended"],
+	extends: ['plugin:@next/next/recommended'],
 
-rules: {
-	"@next/next/no-html-link-for-pages": "off",
-
+	rules: {
+		'@next/next/no-html-link-for-pages': 'off',
+	},
 }
-};
 
-module.exports = config;
+module.exports = config
--- packages/config/tailwind/index.ts
+++ packages/config/tailwind/index.ts
@@ -1,10 +1,10 @@
-import type { Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { fontFamily } from 'tailwindcss/defaultTheme'
+
+import type { Config } from 'tailwindcss'
 
 export default {
-  darkMode: ["class"],
-  content: ["src/**/*.{ts,tsx}", "components/**/*.{ts,tsx}"],
-  theme: {
-  },
-  plugins: [],
-} satisfies Config;
+	darkMode: ['class'],
+	content: ['src/**/*.{ts,tsx}', 'components/**/*.{ts,tsx}'],
+	theme: {},
+	plugins: [],
+} satisfies Config
